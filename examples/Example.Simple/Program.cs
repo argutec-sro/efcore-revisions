e1bc1b0afa15fb397bd2a466c9d22fac979867c5
Michal Holiš <holis@argutec.eu>
2023-07-19 07:30:29 +0000

    .NET 7.0.9

--- examples/Example.Simple/Program.cs
+++ examples/Example.Simple/Program.cs
@@ -12,42 +12,41 @@ namespace Example.Simple
             using (DataContext lDataContext = new DataContext(DesignTimeDbContextFactory.CONNECTION_STRING, new AppContext()))
             {
                 lDataContext.Database.Migrate();
-
-                var lBook = lDataContext.Books.First();
-                lBook.ReleaseDate = lBook.ReleaseDate;
                 
-                // lDataContext.Books.Add(new Book()
-                // {
-                //     ID = Guid.NewGuid(),
-                //     Name = "Lord of the Rings",
-                //     Year = 2000
-                // });
+                lDataContext.Books.Add(new Book()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "Lord of the Rings",
+                    Year = 2000
+                });
 
-                // lDataContext.Books.Add(new Book()
-                // {
-                //     ID = Guid.NewGuid(),
-                //     Name = "Lord of the Flies",
-                //     Year = 2000
-                // });
+                lDataContext.Books.Add(new Book()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "Lord of the Flies",
+                    Year = 2000
+                });
 
-                // lDataContext.Books.Add(new Book()
-                // {
-                //     ID = Guid.NewGuid(),
-                //     Name = "Lord of the Hobbits",
-                //     Year = 2000
-                // });
+                lDataContext.Books.Add(new Book()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "Lord of the Hobbits",
+                    Year = 2000
+                });
 
-                // var lTestBook = new Book()
-                // {
-                //     ID = Guid.NewGuid(),
-                //     Name = "TEST BOOK",
-                //     Year = 2000
-                // };
-                // lDataContext.Books.Add(lTestBook);
+                var lTestBook = new Book()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "TEST BOOK",
+                    Year = 2000
+                };
+                lDataContext.Books.Add(lTestBook);
 
-                // lDataContext.SaveChanges();
+                lDataContext.SaveChanges();
 
 
+                // var lBook = lDataContext.Books.First();
+                // lBook.ReleaseDate = lBook.ReleaseDate;
 
                 // lTestBook.Name = "BOOK HAS BEEN TESTED";
                 // lTestBook.Year = 2020;
